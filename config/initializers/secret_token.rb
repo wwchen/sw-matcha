4cd28daa63cc1bc85ad77a3308b4f6b882ef3371
William Chen <williamchen16@gmail.com>
2013-08-17 19:49:09 -0700

    A wild empty rails appears!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Matcha::Application.config.secret_token = 'df002f56f83bc94011e9d2a8f59e8bdda21b72546a2c909a3c063b3b9f7a0b3f99399c1616be25c87d025cfef21e14ef296f4c9ed1c5e4ac86903cb28580c41d'
